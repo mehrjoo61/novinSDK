d40e190990ec0109727893ae75f98db99f8e2431
farhad <farhadmohammadi175@gmail.com>
2024-03-22 22:38:01 +0330

    Get projectId form window

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const projectId = "65f5e6d61f66367b48fe99eb";
+// @ts-ignore
+const projectId = window.cdpProjectId;
 
 const apiClient = axios.create({
   baseURL: `${process.env.API_URL}/projects/${projectId}`,
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import { registerEvent } from "./events";
 import storage from "./storage";
 import { generateId } from "./utils";
 
-console.log("CDP script loaded :)");
+// @ts-ignore
+if (window.cdpProjectId) {
+  const userId = storage.getItem("user_id");
+  if (!userId) storage.setItem("user_id", generateId(20));
 
-const userId = storage.getItem("user_id");
-if (!userId) storage.setItem("user_id", generateId(20));
-
-registerEvent({
-  userId: storage.getItem("user_id") as string,
-  name: "page_view",
-  properties: { pageUrl: window.location.href },
-});
+  registerEvent({
+    userId: storage.getItem("user_id") as string,
+    name: "page_view",
+    properties: { pageUrl: window.location.href },
+  });
+}
