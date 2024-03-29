5bd49aba0447396ee6fb6aed7cb6ca8b2477626e
farhad <farhadmohammadi175@gmail.com>
2024-03-29 03:32:51 +0330

    Socket

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { io } from "socket.io-client";
 import cookieStorage from "./cookieStorage";
 import { registerEvent } from "./events";
 import { generateId } from "./utils";
@@ -8,19 +9,31 @@ declare global {
   }
 }
 
+// function showNotification(title: string, options?: NotificationOptions) {
+//   Notification.requestPermission().then(() => {
+//     if (Notification.permission === "granted") new Notification(title, options);
+//   });
+// }
+
 if (window.cdpProjectId) {
   const key = "cdp_user_id";
 
   const userId = cookieStorage.getCookieValue(key);
   const oneYearInMilliseconds = 31_536_000_000;
+  const nextYear = new Date(Date.now() + oneYearInMilliseconds);
   if (!userId)
-    document.cookie = `${key}=${generateId(20)}; expires=${new Date(
-      Date.now() + oneYearInMilliseconds
-    )}; path=/`;
+    document.cookie = `${key}=${generateId(20)}; expires=${nextYear}; path=/`;
 
   registerEvent({
     userId: cookieStorage.getCookieValue(key) as string,
     name: "page_view",
     properties: { pageUrl: window.location.href },
+  });
+
+  io(process.env.API_URL!, {
+    query: {
+      projectId: window.cdpProjectId,
+      userId: cookieStorage.getCookieValue(key),
+    },
   });
 }
